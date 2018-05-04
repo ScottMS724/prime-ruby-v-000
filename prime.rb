be6666ed070395371599310e085380722eeb354a
Scott Silverman <scottms724@gmail.com>
2018-05-04 19:41:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(integer)
     return true 
   elsif (integer % 2 != 0 && integer % 3 != 0 && integer % 4 !=0 && integer % 5 != 0 && integer % 6 != 0 && integer % 7 != 0 && integer % 8 != 0 && integer % 9 !=0) 
     return true 
-  elsif (integer <= 1) 
+  elsif integer <= 1
     return false 
   else 
     return false
