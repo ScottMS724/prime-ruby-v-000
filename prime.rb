46c85eb0a3d403639874b1ebc64ee95312224fa5
Scott Silverman <scottms724@gmail.com>
2018-05-04 19:31:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   if (integer == 2 || integer == 3 || integer == 5 || integer == 7)
     return true 
   elsif (integer % 2 != 0 && integer % 3 != 0 && integer % 4 !=0 && integer % 5 != 0 && integer % 6 != 0 && integer % 7 != 0 && integer & 8 != 0 && integer % 9 !=0) 
-      return true 
+    return true 
   else 
     return false 
   end
